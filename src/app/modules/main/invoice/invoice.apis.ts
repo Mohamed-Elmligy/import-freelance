dad0cb370fe9bb4aa33dac21dea9093e4c6335e5
Mohamed Adel <mohamedadelsa4@gmail.com>
2025-03-20 00:26:46 +0200

    finsh delete invoice line

--- src/app/modules/main/invoice/invoice.apis.ts
+++ src/app/modules/main/invoice/invoice.apis.ts
@@ -3,5 +3,6 @@ export const INVOICE_APIS = {
   DELETE_INVOICE: (id: number) => `invoice/${id}/delete`,
   UPDATE_INVOICE: (id: number) => `invoice/${id}/update`,
   GET_INVOICE: (id: number) => `invoice/${id}`,
+  DELETE_INVOICE_LINE: (id: number) => `invoice/delete-line/${id}`,
   GET_INVOICE_FOR_UPDATE: (id: number) => `invoice/${id}/get-for-update`,
 };
