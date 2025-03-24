0d62225e71e66941bbd01fa2836820a6a9cef970
Mohamed Adel <mohamedadelsa4@gmail.com>
2025-03-24 23:01:37 +0200

    chore: update API base URL in environment files for development and production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   // baseUrl: 'http://127.0.0.1:8000/api/',
-  baseUrl: 'http://165.22.23.103:8001/trade_tracker/api/',
+  // baseUrl: 'http://165.22.23.103:8001/trade_tracker/api/',
+  baseUrl: 'http://146.190.27.250/trade_tracker/api/',
   browser_storage_key: "hV(A[=^le`MGL;'$W><|$YRbgupsMt[4<9kAbU#4L:`/NxH1Ra",
 };
