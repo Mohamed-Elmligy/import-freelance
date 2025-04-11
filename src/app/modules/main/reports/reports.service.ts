3669364d1bad66cf225c34542e38dd5e35c0ab0a
Mohamed Adel <mohamedadelsa4@gmail.com>
2025-04-11 20:47:43 +0200

    Remove commented-out sample data from resultsType definition in reports service

--- src/app/modules/main/reports/reports.service.ts
+++ src/app/modules/main/reports/reports.service.ts
@@ -88,33 +88,3 @@ export type resultsType = {
   total_cbm: number;
   total_weight: number;
 };
-
-// {
-//     "totals": {
-//         "box_count": 34,
-//         "item_in_box": 48,
-//         "item_price": 984.0,
-//         "total_price": 1605888.0,
-//         "total_cbm": 0.29,
-//         "total_weight": 136.0
-//     },
-//     "results": [
-//         {
-//             "invoice_number": "342",
-//             "customer_code": "AB53",
-//             "container_sequence": 22,
-//             "item_code": "test",
-//             "item_description": "test test test test",
-//             "box_count": 34,
-//             "item_in_box": 48,
-//             "item_price": 984.0,
-//             "store_cbm": 87.0,
-//             "Width": 85.0,
-//             "Length": 488.0,
-//             "Height": 7.0,
-//             "total_price": 1605888.0,
-//             "total_cbm": 0.29,
-//             "total_weight": 136.0
-//         }
-//     ]
-// }
