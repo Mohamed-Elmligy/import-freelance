f5d446a22c94cba300ae9830d0af68f4e9f8f01f
Mohamed Adel <mohamedadelsa4@gmail.com>
2025-04-23 03:54:02 +0200

    Add shipping cost and exchange rate fields to shipping data form; update API for exchange rates

--- src/app/modules/main/master-data/shipping-data-form/shipping-data-form.component.ts
+++ src/app/modules/main/master-data/shipping-data-form/shipping-data-form.component.ts
@@ -43,14 +43,14 @@ import { MessageModule } from 'primeng/message';
   templateUrl: './shipping-data-form.component.html',
 })
 export default class ShippingDataFormComponent {
-  mainPaths = main_routes_paths;
-  route: MenuItem[] = [];
-  listOfCustomers = signal([]);
-
   private formBuilder = inject(FormBuilder);
   private activatedRoute = inject(ActivatedRoute);
   private lookupService = inject(LookupsService);
   private shippingDataService = inject(ShippingDataService);
+
+  mainPaths = main_routes_paths;
+  route: MenuItem[] = [];
+  listOfCustomers = signal([]);
 
   isUpdate = this.activatedRoute.snapshot.queryParams['edit'] == 'true';
   shippingId = this.activatedRoute.snapshot.queryParams['shippingId'];
@@ -61,25 +61,23 @@ export default class ShippingDataFormComponent {
     shipping_date: string;
     container_sequence: number;
     port_name: string;
+    sea_shipping_cost: number;
+    land_shipping_cost: number;
+    customs_cost: number;
+    usd_exchange_rate: number;
+    rmb_exchange_rate: number;
   } = {
     customer: '',
     container_number: '',
     shipping_date: '',
     container_sequence: 0,
     port_name: '',
+    sea_shipping_cost: 0,
+    land_shipping_cost: 0,
+    customs_cost: 0,
+    usd_exchange_rate: 0,
+    rmb_exchange_rate: 0,
   };
-
-  protected form = this.formBuilder.group({
-    name: [null, [Validators.required]],
-    containerSequence: [
-      null,
-      [Validators.required, Validators.pattern('^[0-9]*$')],
-    ],
-    containerNumber: [null, [Validators.required]],
-    ShippingDate: [null, [Validators.required]],
-    port: [null, []],
-    description: [null, []],
-  });
 
   submit(form: FormGroup) {
     if (form.valid) {
@@ -93,6 +91,11 @@ export default class ShippingDataFormComponent {
   }
 
   ngOnInit() {
+    if (this.isUpdate) {
+      this.getUSDRateData();
+      this.getCNYRateData();
+    }
+
     this.lookupService.getListOfLookups('customers').subscribe((data: any) => {
       this.listOfCustomers.set(data);
       this.shippingDataService.listOfCustomers.set(data);
@@ -109,6 +112,41 @@ export default class ShippingDataFormComponent {
 
     if (this.shippingId && !this.isUpdate) this.getExpenseById();
     if (this.shippingId && this.isUpdate) this.updateExpense();
+  }
+
+  protected form = this.formBuilder.group({
+    name: [null, [Validators.required]],
+    containerSequence: [
+      null,
+      [Validators.required, Validators.pattern('^[0-9]*$')],
+    ],
+    containerNumber: [null, [Validators.required]],
+    ShippingDate: [null, [Validators.required]],
+    port: [null, []],
+    description: [null, []],
+    land_shipping_cost: [null, []],
+    sea_shipping_cost: [null, []],
+    customs_cost: [null, []],
+    usd_exchange_rate: [],
+    rmb_exchange_rate: [],
+  });
+
+  getUSDRateData() {
+    this.shippingDataService.getExchangeRate('USD').subscribe((data: any) => {
+      this.shippingDataService.USDRateData.set(data);
+      this.form
+        .get('usd_exchange_rate')
+        ?.setValue(data.conversion_rates['EGP']);
+    });
+  }
+
+  getCNYRateData() {
+    this.shippingDataService.getExchangeRate('CNY').subscribe((data: any) => {
+      this.shippingDataService.CNYRateData.set(data);
+      this.form
+        .get('rmb_exchange_rate')
+        ?.setValue(data.conversion_rates['EGP']);
+    });
   }
 
   getExpenseById() {
